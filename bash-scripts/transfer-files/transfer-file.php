a172fce07893992c22ffd5c061746b72675c0c27
Brian Lee <brian.lee0820@gmail.com>
2020-02-11 16:10:11 -0500

    Add files via upload

---
+++ bash-scripts/transfer-files/transfer-file.php
@@ -0,0 +1,4 @@
+<!-- Generic .php file for demonstration purposes 
+In a working envrionment, this .php file would be
+transferred over to the remote device then be copied
+over to its final destination.-->
